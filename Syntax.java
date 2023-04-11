4843ff10e428b0518b84d66fb8232ee40fa09ce2
Dhanvanth123 <123087055+Dhanvanth123@users.noreply.github.com>
2023-04-11 06:58:08 -0700

    Update Syntax.java

--- Syntax.java
+++ Syntax.java
@@ -1,6 +1,6 @@
 public class Syntax{
 	public static void main(String[] str){
 		System.out.println("Hello Karthi!!!!!");
-		System.out.println("Hello Systech12234");
+		System.out.println("Hello Systech122345");
 	}
 }
